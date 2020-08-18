f37ad3861778cd2e82317dba3e65e05386b79ead
melonTai <marufuji25@gmail.com>
2020-08-18 21:35:07 +0900

    getCross やっぱあってた

--- xrot_to_fusion_sketch/xrot_to_fusion_sketch.py
+++ xrot_to_fusion_sketch/xrot_to_fusion_sketch.py
@@ -277,11 +277,8 @@ class PropDesign():
         a = [p2[0] - p1[0], p2[1] - p1[1]]
         b = [p4[0] - p3[0], p4[1] - p3[1]]
         c = [p3[0]-p1[0],p3[1]-p1[1]]
-        absb = (b[0]**2 + b[1]**2)**0.5
-        m = self.cross(b, c)/absb
-        n = self.cross(b, a)/absb
 
-        return [p3[0] + b[0] * m / (m + n) , p3[1] + b[1] * m / (m + n)]
+        return [p1[0] + a[0] * self.cross(b,c)/self.cross(b, a) , p1[1] + a[1] * self.cross(b,c)/self.cross(b, a)]
 
     def removeCross(self,ps):
         """
